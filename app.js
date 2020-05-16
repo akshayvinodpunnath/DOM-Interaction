0d850aeeee3952beb25fbb4b3651b1e7c2dca4e0
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-17 00:35:53 +0530

    getElementsByTagName added for li

---
+++ app.js
@@ -0,0 +1,6 @@
+//const listItemElements = document.querySelectorAll('li');
+const listItemElements = document.getElementsByTagName('li');
+
+for ( const listItem of listItemElements) {
+    console.log(listItem)
+}
