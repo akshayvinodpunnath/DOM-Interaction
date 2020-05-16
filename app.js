e880534ca216f22e48d6c0336b9734d4b193e4da
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-17 00:38:32 +0530

    getElementById for main-title id, textarea, color and backgroundcolor modified

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 //const listItemElements = document.querySelectorAll('li');
+const h1 = document.getElementById('main-title');
 const listItemElements = document.getElementsByTagName('li');
+
+h1.textContent = 'DOM - Manipulated';
+h1.style.color = 'purple';
+h1.style.backgroundColor = 'yellow';
 
 for ( const listItem of listItemElements) {
     console.log(listItem)
